cedd1579e082fc458b70f26c6f3f312cb02dc36d
Suresh-inbit <suresh.inbit@gmail.com>
2025-06-04 22:40:01 +0530

    commiting to edit in other system

--- train/train_gan.py
+++ train/train_gan.py
@@ -18,33 +18,50 @@ from models.generator import Generator
 from models.discriminator_cnn import Discriminator_cnn
 import torchvision
 from torchvision4ad.datasets import MVTecAD
-
-def train_gan_cnn(epochs=10, batch_size=16, noise_dim=128, lr=0.0002):
+torch.manual_seed(99)
+def train_gan_cnn(epochs=50, batch_size=2, noise_dim=128, lr=0.0002):
     device = "cuda" if torch.cuda.is_available() else "cpu"
     transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Grayscale(),
-        transforms.Normalize([0.5], [0.5]),
-        transforms.Resize((256, 256)),  # Reduce image size to save memory
+        transforms.Normalize([0.5], [0.5]), # normalize image with mean and standard deviation.
+        transforms.Resize((512, 512)),  # Reduce image size to save memory
     ])
+
+    def weights_init(m):
+        classname = m.__class__.__name__
+        if classname.find('Conv') != -1:
+
+            nn.init.normal_(m.weight.data, 0.0, 0.02)
+        elif classname.find('BatchNorm') != -1:
+            nn.init.normal_(m.weight.data, 0.0, 0.02)
+            nn.init.constant_(m.bias.data, 0)
+
     dataloader = DataLoader(MVTecAD("MVTec", 'grid', train =True, transform=transform, download=True),
-                          batch_size=batch_size, shuffle=True, pin_memory=False, num_workers=4)
+                          batch_size=batch_size, shuffle=True, pin_memory=True, num_workers=4)
+    sample_img = next(iter(dataloader))[0].to(device)
     img_shape = dataloader.dataset[0][0].shape
-    print("size of images: ", img_shape)
+    print("size of images: ", sample_img.shape)
     # exit(0)
     # Models
-    netG = Generator_cnn(noise_dim, img_shape).to(device)
+    netG = Generator_cnn(noise_dim, img_shape, 128).to(device)
     netD = Discriminator_cnn(img_shape).to(device)
-    print(netG)
-    print("number of parameters in generator: ", sum(p.numel() for p in netG.parameters()), f"| Memory occupied: {sum(p.numel() for p in netG.parameters())/(2**17)}MB")
-    fixed_noise = torch.randn(batch_size, noise_dim, 1, 1, device=device)
-    print("number of parameters in D: ", sum(p.numel() for p in netD.parameters()),  f"| Memory occupied: {sum(p.numel() for p in netD.parameters())/(2**17)}MB")
 
-    # print(fixed_noise.shape)
-    # pred = netG(fixed_noise)
-    # print("pred shape: ", pred.shape)
-    # out = netD(pred)
-    # print("out shape: ", out.shape, "Output : ", out[0])
+    netG.apply(weights_init)
+    netD.apply(weights_init)
+
+    print(netG)
+    print("Parameter dtype: ",next(netG.parameters()).dtype)
+    print("number of parameters in generator: ", sum(p.numel() for p in netG.parameters()), f"| Memory occupied: {sum(p.numel() for p in netG.parameters())/(2**18)} MB")
+    fixed_noise = torch.randn(batch_size, noise_dim, 1, 1, device=device)
+    print("number of parameters in D: ", sum(p.numel() for p in netD.parameters()),  f"| Memory occupied: {sum(p.numel() for p in netD.parameters())/(2**18)} MB")
+
+    print("Noise shape: ",fixed_noise.shape)
+    pred = netG(fixed_noise)
+    print("pred shape: ", pred.shape)
+
+    out = netD(sample_img)
+    print("out shape: ", out.shape, "Output : ", out[0])
     # print(netD)
     # exit(0)
 
@@ -55,12 +72,12 @@ def train_gan_cnn(epochs=10, batch_size=16, noise_dim=128, lr=0.0002):
     fake_label = 0.
 
     # Setup Adam optimizers for both G and D
-    optimizerD = optim.Adam(netD.parameters(), lr=lr, betas=(0.9, 0.999))
-    optimizerG = optim.Adam(netG.parameters(), lr=lr, betas=(0.9, 0.999))
+    optimizerD = optim.Adam(netD.parameters(), lr=lr, betas=(0.5, 0.999))
+    optimizerG = optim.Adam(netG.parameters(), lr=lr, betas=(0.5, 0.999))
 
     D_losses = []
     G_losses = []
-    img_list = []
+    # img_list = []
     #Training Loop
     for epoch in range(epochs):
         for i, data in enumerate(dataloader, 0):
@@ -75,10 +92,12 @@ def train_gan_cnn(epochs=10, batch_size=16, noise_dim=128, lr=0.0002):
             output = netD(img).view(-1)
             # print("Output_D shape :",output.shape, 'label shape:' , label.shape)
             # exit(0)
+            # print(output)
             error_real = loss(output, label)
             error_real.backward()
-
+            # optimizerD.step()
             noise = torch.randn(b_size, noise_dim, 1, 1, device=device)
+            # print(noise.shape)
             fake = netG(noise)
 
             label.fill_(fake_label)
@@ -91,7 +110,7 @@ def train_gan_cnn(epochs=10, batch_size=16, noise_dim=128, lr=0.0002):
 
             # (2) Update G network: 
             netG.zero_grad()
-            label.fill_(real_label)  # fake labels are real for generator cost
+            label.fill_(real_label)  #  labels are real for generator cost est
             output = netD(fake).view(-1)
             error_G = loss(output, label)
             error_G.backward()
@@ -99,27 +118,30 @@ def train_gan_cnn(epochs=10, batch_size=16, noise_dim=128, lr=0.0002):
 
             if i % 50 ==0:
                 print( f"Epoch :{epoch} [{i}/264] LOSS: {error_G} , {error_D}")
-            D_losses.append(error_D)
-            G_losses.append(error_G)
+            D_losses.append(error_D.item())
+            G_losses.append(error_G.item())
 
         with torch.no_grad():
             pred = netG(fixed_noise).detach().cpu()
-        img_list.append(torchvision.utils.make_grid(fake, padding=2, normalize=True))
-
+        img_save =torchvision.utils.make_grid(fake, padding=2,nrow=4, normalize=True)
+        torchvision.utils.save_image(img_save, f"./images/mv/img_{epoch}.png")
+    torch.save(netG.state_dict(), "./gen.pth")
+    torch.save(netD.state_dict(), "./Des.pth")
     plt.figure(figsize=(10,5))
     plt.title("Generator and Discriminator Loss During Training")
-    plt.plot(G_losses,label="G")
-    plt.plot(D_losses,label="D")
+    plt.plot(np.arange(len(G_losses)), G_losses ,label="G")
+    plt.plot(np.arange(len(D_losses)), D_losses,label="D")
     plt.xlabel("iterations")
     plt.ylabel("Loss")
     plt.legend()
-    plt.show()
+    # plt.show()
+    plt.savefig("./images/graph.png")
     
-    plt.imshow(img_list[0])
-    plt.show()
-    i=0
-    for img in img_list:
-        torchvision.utils.save_image(img, f"img_{i}.png")
+    # plt.imshow(img_list[0].cpu())
+    # plt.show()
+    # i=0
+    # for img in img_list:
+    #     torchvision.utils.save_image(img, f"./images/mv/img_{i}.png")
 
 
 def train_gan(
